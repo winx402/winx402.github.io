fdcea86bdb845c55526d52846e89ad6536841352
wangwenxiang <wangwenxiang1993@gmail.com>
2017-01-21 22:07:59 +0800

    Site updated: 2017-01-21 22:07:58

--- js/base.js
+++ js/base.js
@@ -4,12 +4,6 @@ $(document).ready(function ($) {
         return false
     });
 });
-NProgress.configure({showSpinner: false, minimum: 0.1});
-NProgress.start();
-$(window).load(function () {
-    NProgress.done(true);
-    $('.fade').removeClass('out');
-});
 
 $(".guide-button").click(function () {
     location.href = $(this).attr("_href");
