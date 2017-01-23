cbcf22203fd7a562a6e1449d2a85b8384b3cb94d
wangwenxiang <wangwenxiang1993@gmail.com>
2017-01-23 15:58:27 +0800

    Site updated: 2017-01-23 15:58:27

--- js/base.js
+++ js/base.js
@@ -1,10 +1,15 @@
-$(document).ready(function ($) {
-    $("a#gotop").click(function () {
-        $("html,body").animate({scrollTop: "0px"}, 600);
-        return false
-    });
-});
-
 $(".guide-button").click(function () {
     location.href = $(this).attr("_href");
+});
+
+$(".book-post").click(function () {
+    location.href = $(this).attr("_href");
+});
+
+$("#gotop").click(function(){
+    $("#wrap").animate({scrollTop:"0px"},600);
+});
+
+$("#info-github").click(function(){
+    window.open($(this).attr("_href"));
 });
