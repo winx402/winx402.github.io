8d67f4e554d6abd34f722ad178bbc74955ef1e2a
wangwenxiang <wangwenxiang1993@gmail.com>
2017-02-09 20:10:49 +0800

    test

--- submit.sh
+++ submit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "**********start commit************\n\n" &&
+echo "***********************************************start commit" &&
 git commit -am $1 &&
-echo "\n\n**********commit complete************\n\n" &&
-echo "\n\n**********push start************\n\n" &&
+echo "***********************************************commit complete" &&
+echo "***********************************************push start" &&
 git push
-echo "\n\n**********push complete************\n\n" &&
+echo "***********************************************push complete" &&
